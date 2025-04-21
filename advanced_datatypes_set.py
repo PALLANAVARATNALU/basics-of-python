235e76681b742daac9e2f2e7e163e7c92ae40619
PALLANAVARATNALU <134999557+PALLANAVARATNALU@users.noreply.github.com>
2025-04-21 21:44:24 +0530

    Add files via upload

---
+++ advanced_datatypes_set.py
@@ -0,0 +1,14 @@
+# Set Homogenous & Heterogenous
+x = {10,20,30,40,50}
+print(x)
+print(len(x))
+print(type(x))
+
+# adding elements to set (mutable)
+y={(10,20,30),True,1,2,5,6.7,18+9j,"program"}
+print(y)
+print(len(y))
+y.add(350)
+print(y)
+print(len(y))
+s=set()  # declaration of empty set
