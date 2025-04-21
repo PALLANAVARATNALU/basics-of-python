6dcfe3bdc2549015c2836f6029f9b73d6b2ea359
PALLANAVARATNALU <134999557+PALLANAVARATNALU@users.noreply.github.com>
2025-04-21 13:28:15 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,40 @@
+#  Python operators used to perform operations between values
+# 1.Arithemetic Operator(+,-,*,%,/,//)
+# 2.Assignment Operator(=,!=)
+# 3.Comparison Operator (==,>=,<=,!=,>,<)
+# 4.logical Operator (AND,OR,NOT)
+
+
+# Arithemetic operator(+,-,*,/,%,//)
+
+a=5
+b=6
+print(a+b)    # addition
+print(a-b)    # subtraction
+print(a*b)    # multiplication
+print(a/b)    # division
+print(a%b)    # modules
+print(a//b)   #float division
+
+
+# Assignment Operator(=,!=)
+x=5
+print(x)
+# here x is variable
+# = is assignment operator
+# 5 is a value
+
+
+# comparision operator(>,<,==,!=,>=,<=)
+a=10
+b=20
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a<=b)
+print(a>b)
+print(a<b)
+
+# Logical Operator (AND,OR,NOT)
+print(4 and 5)
+print(4 or 5)
