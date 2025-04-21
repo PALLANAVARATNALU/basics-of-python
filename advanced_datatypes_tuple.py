c6121c38436e2b12cb9e8c1393d49c4a46f40158
PALLANAVARATNALU <134999557+PALLANAVARATNALU@users.noreply.github.com>
2025-04-21 21:22:29 +0530

    Add files via upload

---
+++ advanced_datatypes_tuple.py
@@ -0,0 +1,18 @@
+# Tuple packing and unpacking
+# Tuple Homogenous
+t=(10,20,30,40)
+print(type(t))
+t1=10,20,30,40
+print(type(t1))
+
+# Tuple Heterogenous
+t2=("python",10,2.6,10+6j,True,20)
+print(t2[-3:])
+
+# Fetching elements from tuple using indexing
+numb = (10, 20, 30, 40, 50, 60, 70, 80, 90)
+print(numb[0::2])
+print(numb[1::2])
+print(numb[-1::-2])
+print(numb[-2::-2])
+print(numb[1:5])
