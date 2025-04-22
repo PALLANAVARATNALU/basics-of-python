a615214903a2ec8f8756155df3e10bdacfd5f40c
PALLANAVARATNALU <134999557+PALLANAVARATNALU@users.noreply.github.com>
2025-04-22 14:50:45 +0530

    Add files via upload

---
+++ advanced_datatypes_dict.py
@@ -0,0 +1,16 @@
+# Dictionary is used to store pair of elements,stores keypair values
+
+#example: print basic data as dictionary
+dict1={"name":"rossom","place":"california","designation":"developer","salary":"40000"}
+dict2={"name":"iyer","place":"punjab","designation":"captain","ismarried":False}
+print(dict2["ismarried"])     # results false
+print(dict2["name"])         # results iyer
+print(dict2["place"])        # results punjab
+print(dict2["designation"])  # results captain
+print(dict1["name"])         # results rossom
+print(dict1["place"])        # results california
+print(dict1["designation"])  # results developer
+print(dict1["salary"])      # results 40000
+
+
+
