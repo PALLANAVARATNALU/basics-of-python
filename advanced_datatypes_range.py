d21b3a6fc895ee629015d8473e1ae158869cd212
PALLANAVARATNALU <134999557+PALLANAVARATNALU@users.noreply.github.com>
2025-04-22 14:37:08 +0530

    Add files via upload

---
+++ advanced_datatypes_range.py
@@ -0,0 +1,13 @@
+# Range (to generate numbers)
+
+
+x=range(20)
+print(x) # result only range(0,20)
+
+# range to generate numbers
+x=list(range(0,20))
+print(x) # list of numbers from 0 to 19
+
+# range to generate  Alt numbers by using stepsize
+x=set(range(2,200,2))
+print(x) # results altenerate numbers as set from 2 to 198
