a5364643ac3e9d48485082e06be372016058260f
PALLANAVARATNALU <134999557+PALLANAVARATNALU@users.noreply.github.com>
2025-04-25 11:30:36 +0530

    Add files via upload

---
+++ loops-while.py
@@ -0,0 +1,28 @@
+# Loops-if we want to perform any task repeatedly
+# Types of loops:
+# While loop
+# For loop
+
+# Syntax for While loop:
+# While Boolean expression:
+# // Statement
+
+
+# Read the number from keyboard and print even numbers or odd numbers as per input in first 100 natural numbers
+x=int(input("enter the number:"))
+while x<=100:
+    print(x,end=" ")
+    x+=2
+
+
+
+# print sum  of numbers in first 100 natural numbers
+sum1=0
+num=1
+while num<=100:
+    sum1+=num
+    num=num+1
+print(sum1)
+
+
+
